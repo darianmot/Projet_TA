98faa34b593c2c98cd6e344350fbca7c74bc8a94
AlexandreAndreazza <alexandre.andreazza@orange.fr>
2017-01-09 10:45:45 +0100

    new sequencement

--- sequencement.ml
+++ sequencement.ml
@@ -16,7 +16,7 @@ let impr = fun tab f ->
   let size = Array.length tab in
   for i = 0 to (size-1) do
     let flight = tab.(i) in
-    Printf.printf "%d\n" (f flight) done;;
+    Printf.printf "%s\n" (f flight) done;;
 
 let impr2 tab = 
   let size = Array.length tab in
@@ -30,14 +30,35 @@ let cmp_tmin = fun v1 v2 ->
   
 let load () = read "data/lfpg_flights.txt";;
 (* Charge la liste des vols du fichier _file (global) *)
+  
 
 let tableau = list_to_tab (load ());;
 (*passe la liste des vols en tableau *)
 
-Array.sort cmp_tmin tableau;;
+let list_runway = fun tab ->
+  let acc26L = ref [] in
+  let acc26R = ref [] in
+  let acc27L = ref [] in
+  let acc27R = ref [] in
+  for i=0 to ((Array.length tab)-1) do
+    match (get_runway tab.(i)) with
+	"26L" -> acc26L := tab.(i)::(!acc26L)
+      |"26R" -> acc26R := tab.(i)::(!acc26R)
+      |"27L" -> acc27L := tab.(i)::(!acc27L)
+      |"27R" -> acc27R := tab.(i)::(!acc27R)
+      |_ -> ();
+  done;
+  let res = [| !acc26L; !acc26R; !acc27L; !acc27R |] in
+  res;;
+
+let tri = list_runway tableau;;
+let rwy26L = Array.sort cmp_tmin (list_to_tab tri.(0));;
+let rwy26R = Array.sort cmp_tmin (list_to_tab tri.(1));;
+let rwy27L = Array.sort cmp_tmin (list_to_tab tri.(2));;
+let rwy27R = Array.sort cmp_tmin (list_to_tab tri.(3));;
 
 
-let sequence = fun retard i tab ->
+let sequence_opti = fun retard i tab ->
   let borne = ref max_int in
   let n =  Array.length tab in
   let solution = ref (Array.copy tab) in
@@ -54,18 +75,15 @@ let sequence = fun retard i tab ->
 	then
           begin
 	    tab.(i) <- (change_t_eff tab.(i) (get_t_rwy tab.(i)));
-            (*Printf.printf "%s : teff = %d t_rwy = %d\n" (getCallsign tab.(i)) (get_t_eff tab.(i)) (get_t_rwy tab.(i));*)
           end
 	else
 	  begin
 	    tab.(i) <- change_t_eff tab.(i) (max (get_t_rwy tab.(i)) (get_t_eff tab.(i-1) + (separation tab.(i-1) tab.(i))));
-	    (*Printf.printf "%s : teff = %d t_rwy = %d\n" (getCallsign tab.(i)) (get_t_eff tab.(i)) (get_t_rwy tab.(i));*)
 	  end;
 	let retardi  = retard + get_t_eff tab.(i) - get_t_rwy tab.(i) in
 	if retardi < !borne
 	then
-       	    sequence_rec retardi (i+1)
-        else
+       	    sequence_rec retardi (i+1);
           begin
 	    if i != j
 	    then
@@ -82,16 +100,24 @@ let sequence = fun retard i tab ->
   in sequence_rec retard i;
   !solution;;
 
+
 let tableau_sub = Array.sub tableau 0 8;;
-let solution = sequence 0 0 tableau_sub;;
+let solution = sequence_opti 0 0 tableau_sub;;
 impr2 solution;;
 
+let sequence_fifo = fun tab ->
+  for i = 0 to Array.length tab -1 do
+    if i = 0 then 
+      tab.(i) <- change_t_eff tab.(i) (get_t_rwy tab.(i));
+    else 
+      tab.(i) <- change_t_eff tab.(i) (max (get_t_rwy tab(i)) (get_t_rwy tab.(i-1) + separation tab.(i-1) tab.(i))
+   done;
+   tab;;
 
-
-(*
-let get_New_t_debut tab ->
-  for i = 0 to Array.length tab
-  do
-    get_h_
-*)
+let sequence_rwy = fun tab n ->
+  let tab_sub = Array.make n 0 in
+  let reste = (Array.length tab) mod n in
+  let quotient = (Array.length tab) / n in
+  for i=0 to quotient do
+    for j 
 
